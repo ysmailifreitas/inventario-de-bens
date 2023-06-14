a2b1cd381912c6da2651659740ddc1cb83a4a0a7
Guilsimi <guilherme.simioni04@gmail.com>
2023-06-14 16:51:50 -0300

    Confirmação ao excluir item ou fornecedor

---
+++ public/js/fornecedores.js
@@ -0,0 +1,9 @@
+function removerFornecedor(){
+var a = document.querySelector("#deletar")
+
+var confirmarAcao = confirm("Deseja deletar o fornecedor?")
+
+if(confirmarAcao == false){
+    a.href = "/fornecedores"
+}
+}
---
+++ public/js/itens.js
@@ -0,0 +1,9 @@
+function removerItem(){
+    var a = document.querySelector("#deletar")
+    
+    var confirmarAcao = confirm("Deseja deletar o item?")
+    
+    if(confirmarAcao == false){
+        a.href = "/itens"
+    }
+    }
