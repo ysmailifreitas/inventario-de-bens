9eeb3b6412040b82c8261c1f7647b3625493b076
ysmailifreitas <ysmailigabriel@gmail.com>
2024-03-15 13:12:33 -0400

    ajustes do merge

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("inventario", "teste", "password", {
+const sequelize = new Sequelize("inventario", "inventario_user", "password", {
     host: 'localhost',
     // host: 'mysql-container',
     dialect: 'mysql'
