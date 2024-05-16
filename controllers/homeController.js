3e4c03d85ad11fab2dd883c3e2183dd4f7ae1dd7
ysmailifreitas <ysmailigabriel@gmail.com>
2024-05-16 16:10:14 -0400

    responsividade tabelas

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,4 +1,4 @@
-const { Sequelize, Model, DataTypes } = require("sequelize");
+const {Sequelize, Model, DataTypes} = require("sequelize");
 const Itens = require("../models/Patrimonio");
 const Fornecedor = require("../models/Fornecedores");
 const {Usuarios} = require("../models/Usuarios");
@@ -18,19 +18,21 @@ exports.getHomePage = async (req, res) => {
 }
 
 exports.getHomeData = async (req, res) => {
-  try {
-    const total_itens = await Itens.count();
-    const total_fornecedores = await Fornecedor.count();
-    const total_usuarios = await Usuarios.count();
-    const preco = await Itens.sum('pat_valor');
-    const preco_total = preco.toFixed(2);
-    res.render('home', { total_usuarios, total_itens, total_fornecedores, preco_total });
-  } catch (error) {
-    console.error('Erro ao fazer contagens:', error);
-    res.status(500).send('Erro ao fazer contagens');
-  }
+    try {
+        const total_itens = await Itens.count();
+        const total_fornecedores = await Fornecedor.count();
+        const total_usuarios = await Usuarios.count();
+        const preco = await Itens.sum('pat_valor');
+        const preco_total = preco.toFixed(2);
+        let usuarioLogado = await Usuarios.findOne({where: {usr_nome: req.session.username}});
 
-  Itens.findAll().then(function (itens) {
-          res.render("home", {itens, username: req.session.username});
-      });
+        res.render('home', {total_usuarios, total_itens, total_fornecedores, preco_total, username: usuarioLogado});
+    } catch (error) {
+        console.error('Erro ao fazer contagens:', error);
+        res.status(500).send('Erro ao fazer contagens');
+    }
+
+    // Itens.findAll().then(function (itens) {
+    //         res.render("home", {itens, username: req.session.username});
+    //     });
 };
